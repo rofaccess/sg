ffb3e9d8938631dcf9d4c78b4cb785944083f78e
rofaccess <rofaccess@gmail.com>
2013-08-16 17:37:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sg::Application.config.secret_key_base = '47fed6ef8fcd915ea459ec182bef3288884c26665704e4602ccb63c902e4b645497e693abf5cef357c2897125c24f0f338272fc59f88beed8c098002e0c81c13'
