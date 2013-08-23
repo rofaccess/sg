e93d49b97089c2ab611f1d2c0287dc11430d2a81
carlosleite <carlosleitebos@gmail.com>
2013-08-22 23:44:12 -0400

    Remove welcome controller and added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Sg::Application.routes.draw do
   devise_for :users
-  get "welcome/index"
+  get "pages/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -41,7 +41,7 @@ Sg::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
