ff4ca9f711f8bd2bc66f0e955b4cd61f44f45e8d
Ruben84 <rubenquintana1984@gmail.com>
2013-08-30 09:56:20 -0400

    Update Messages of Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,32 +18,36 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to products_path, notice: t('messages.provider_saved')
+      redirect_to products_path, notice: t('messages.product_saved')
     else
-      redirect_to products_path, alert: t('messages.provider_not_saved')
+      redirect_to products_path, alert: t('messages.product_not_saved')
     end
 
   end
 
   def update
     if @product.update(product_params)
-      redirect_to products_path, notice: t('messages.provider_saved')
+      redirect_to products_path, notice: t('messages.product_saved')
     else
-      redirect_to products_path, alert: t('messages.provider_not_saved')
+      redirect_to products_path, alert: t('messages.product_not_saved')
     end
   end
-  
+
   def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url }
+    if @product.destroy
+      respond_to do |format|
+        format.html { redirect_to products_url, notice: t('messages.product_deleted') }
+        format.json { head :no_content }
+      end
+    else
+      format.html { redirect_to products_url, notice: t('messages.product_not_deleted') }
       format.json { head :no_content }
     end
   end
 
   def set_product
     @product = Product.find(params[:id])
-  end  
+  end
 
   def product_params
       params.require(:product).permit(:name, :mark, :unit_price, :provider_id)
