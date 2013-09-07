75b45d3218497b12600ebbfbc17fbec6a351254b
carlosleite <carlosleitebos@gmail.com>
2013-09-07 10:36:53 -0400

    Seed para cargar proveedores y productos

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'http://rubygems.org'
 
+gem 'faker'
+
+gem 'pg'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'admin', password: 'admin', password_confirmation: 'admin')
+
+for i in 1..20
+	Provider.create(	name: Faker::Company.name,
+						ruc: Faker::Number.number(9),
+						address: Faker::Address.street_address,
+						phone: Faker::PhoneNumber.phone_number,
+						email: Faker::Internet.email)
+end
+
+prov = Provider.all
+prov.each do |p|
+	for i in 1..10
+		Product.create(name: Faker::Commerce.product_name, provider_id: p.id)
+	end
+end
