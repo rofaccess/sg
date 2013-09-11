cafd717d1ebd1b31202092cedc2907318bd19df2
Ruben84 <rubenquintana1984@gmail.com>
2013-09-11 14:56:30 -0400

    Modificaciones de configuraciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+	before_filter :set_configuraciones
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_configuraciones
+     @configuraciones = Configuracion.all
+
+     @configuraciones.each do |p|
+	    @nombre = p.nombre
+		@direccion = p.direccion
+	    @imagen = p.imagen.url(:thumb)
+	 end
+  end
 end
