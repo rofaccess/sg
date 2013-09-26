0a2d255157e0a872341986aa60b5e70cbe70626b
carlosleite <carlosleitebos@gmail.com>
2013-09-26 00:40:58 -0400

    Arreglado relacion entre proveedores y componente_categorias

--- app/models/personas/proveedor.rb
+++ app/models/personas/proveedor.rb
@@ -1,4 +1,4 @@
 class Proveedor < Persona
-	has_and_belongs_to_many :componente_categorias
+	has_and_belongs_to_many :componente_categorias, join_table: 'componentes_categorias_proveedores'
 	has_many :facturas_compra
 end
