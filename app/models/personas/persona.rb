9c091895655a05b9c7ae5f5f721848a7120f6d91
rofaccess <rofaccess@gmail.com>
2013-09-29 23:03:43 -0400

    #62 Creado el modelo telefonos con sus relaciones re#62

--- app/models/personas/persona.rb
+++ app/models/personas/persona.rb
@@ -1,5 +1,8 @@
 class Persona < ActiveRecord::Base
 	belongs_to :ciudad
+	has_many   :telefonos
+
+	accepts_nested_attributes_for :telefonos
 
 	# validates :nombre, :ruc, :direccion, :email, :ciudad_id, presence: true
 end
