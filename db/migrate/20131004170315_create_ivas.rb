792338c9991a9feb04e25ed6ece22bc891028a33
rofaccess <rofaccess@gmail.com>
2013-10-18 00:50:51 -0300

    #25 Actualizacion de iva  re#25

--- db/migrate/20131004170315_create_ivas.rb
+++ db/migrate/20131004170315_create_ivas.rb
@@ -1,8 +1,7 @@
 class CreateIvas < ActiveRecord::Migration
   def change
     create_table :ivas do |t|
-      t.string  :abreviatura, null: false, limit: 5
-      t.decimal :porcentaje,  null: false, precision: 10, scale: 2
+      t.string  :valor, null: false, limit: 4
       t.string  :descripcion, null: true, limit: 50
 
       t.timestamps
