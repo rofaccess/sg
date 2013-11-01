fa32022833663ed5c1cb8015c0dae7a89fefda2c
Carlos Leite <carlosleitebos@gmail.com>
2013-11-01 14:21:39 -0300

    #89 #100 Actualizado generar orden de compra. Arregldo errores con 0 y nil re#89 re#100

--- app/models/pedido_compra_detalle.rb
+++ app/models/pedido_compra_detalle.rb
@@ -8,7 +8,7 @@ class PedidoCompraDetalle < ActiveRecord::Base
 		cotizaciones = self.pedido_cotizacion_detalles
 		cotizados = []
 		cotizaciones.each do |c|
-			cotizados.push(c) if c.pedido_cotizacion.estado == PedidosEstados::COTIZADO
+			cotizados.push(c) if c.pedido_cotizacion.estado == PedidosEstados::COTIZADO && !c.costo_unitario.nil? && c.costo_unitario != 0
 		end
 		cotizados
 	end
