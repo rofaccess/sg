1823b7d6e05d96cd4d5e15a15bdc5c08e141f655
carlosleite <carlosleitebos@gmail.com>
2013-11-01 14:59:46 -0400

    Fixed metodo proveedores de detalles de cotizaciones

--- app/models/pedido_cotizacion_detalle.rb
+++ app/models/pedido_cotizacion_detalle.rb
@@ -4,6 +4,6 @@ class PedidoCotizacionDetalle < ActiveRecord::Base
   belongs_to :pedido_compra_detalle
 
   def proveedores
-  	PedidoCotizacionDetalle.includes(:pedido_cotizacion).where('pedidos_cotizacion.estado == ?', 'Cotizado').where('costo_unitario <> ?', 0).where(pedido_compra_detalle: self.pedido_compra_detalle, componente_id: self.componente_id)
+  	PedidoCotizacionDetalle.includes(:pedido_cotizacion).where('pedidos_cotizacion.estado = ?', 'Cotizado').where('costo_unitario <> ?', 0).where(pedido_compra_detalle: self.pedido_compra_detalle, componente_id: self.componente_id)
   end
 end
