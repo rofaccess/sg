8fc383cfd06b4ebffbaad9af932a928c9ae6b164
Carlos Leite <carlosleitebos@gmail.com>
2013-11-09 17:02:20 -0300

    #109 Implementado acts_as_paranoid en plazo_pago e iva. re#109

--- app/models/mercaderias/mercaderia.rb
+++ app/models/mercaderias/mercaderia.rb
@@ -1,5 +1,5 @@
 class Mercaderia < ActiveRecord::Base
 	protokoll :codigo, pattern: '#####'
 
-	belongs_to :iva
+	belongs_to :iva, with_deleted: true
 end
--- app/models/plazo_pago.rb
+++ app/models/plazo_pago.rb
@@ -2,5 +2,7 @@ class PlazoPago < ActiveRecord::Base
 	has_many :factura_compras
 	has_many :plazo_pago_detalles
 
+	acts_as_paranoid
+
 	accepts_nested_attributes_for :plazo_pago_detalles, reject_if: :all_blank, allow_destroy: true
 end
