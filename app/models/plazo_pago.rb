b172c698aa36d68bcdc926105adb0c49aacc4c63
Carlos Leite <carlosleitebos@gmail.com>
2013-11-09 12:55:06 -0300

    #26 Agregado ABM para configuracion de facturacion. re#26

--- app/models/plazo_pago.rb
+++ app/models/plazo_pago.rb
@@ -1,3 +1,6 @@
 class PlazoPago < ActiveRecord::Base
 	has_many :factura_compras
+	has_many :plazo_pago_detalles
+
+	accepts_nested_attributes_for :plazo_pago_detalles, reject_if: :all_blank, allow_destroy: true
 end
