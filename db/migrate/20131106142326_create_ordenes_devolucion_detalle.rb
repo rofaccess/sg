425e6a2052a24aa105d0b5e223bdf7c6085cdbc6
Ruben84 <rubenquintana1984@gmail.com>
2013-11-12 09:25:38 -0300

    #27 agregado los campos de relacion en las migraciones de ordenes de devolucion re#27

--- db/migrate/20131106142326_create_ordenes_devolucion_detalle.rb
+++ db/migrate/20131106142326_create_ordenes_devolucion_detalle.rb
@@ -7,13 +7,13 @@ class CreateOrdenesDevolucionDetalle < ActiveRecord::Migration
       t.decimal  :costo_unitario                  ,default: 0
       t.string   :iva                             ,default: 0
       t.string   :motivo                          ,default: '' ,limit: Domain::DESCRIPCION
-      t.integer :factura_compra_detalle_id         ,null: false
+      t.integer :orden_compra_detalle_id         ,null: false
 
       t.timestamps
     end
 
     add_foreign_key(:ordenes_devolucion_detalle, :ordenes_devolucion, column: 'orden_devolucion_id', options: 'ON DELETE CASCADE')
     add_foreign_key(:ordenes_devolucion_detalle, :mercaderias, column: 'componente_id', options: 'ON DELETE RESTRICT')
-    add_foreign_key(:ordenes_devolucion_detalle, :facturas_compra_detalle, column: 'factura_compra_detalle_id', options: 'ON DELETE RESTRICT')
+    add_foreign_key(:ordenes_devolucion_detalle, :ordenes_compra_detalle, column: 'orden_compra_detalle_id', options: 'ON DELETE RESTRICT')
   end
 end
