dc51a999ccb934be218193819acdf4e25447d273
Carlos Leite <carlosleitebos@gmail.com>
2013-11-24 23:02:04 -0300

    Minor fix

--- app/controllers/pedidos_compra_controller.rb
+++ app/controllers/pedidos_compra_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class PedidosCompraController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_pedido_compra, only: [:show, :update, :destroy]
