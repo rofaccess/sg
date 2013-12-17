a86dffec3a65b850d3281b291d6d9ad54e49c348
Carlos Leite <carlosleitebos@gmail.com>
2013-12-17 11:44:02 -0300

    Actualizado time zone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Sg
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Buenos Aires'
+    config.time_zone = 'America/Asuncion'
     #
     # Custom directories with classes and modules you want to be autoloadable.
 
